a9e5be4899174151af26601e76d6a08d709aad8e
sarawildbret1 <74148466+sarawildbret1@users.noreply.github.com>
2021-01-15 13:34:18 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+
+
+
+//var menuButton = document.querySelector(".hamburgerbutton");
+//var nav = document.querySelector(".tovermenu");
+
+//function  changeButton() {
+    //nav.classList.toggle(".tovermenu");
+    //menuButton.classList.toggle(".displaynone");
+ //}
+
+//menuButton.addEventListener("click", changeButton);
+
+var menuButton = document.querySelector(".hamburgerbutton");
+var nav = document.querySelector(".displaynone");
+
+menuButton.addEventListener("click", () => {
+    nav.classList.toggle("tovermenu");
+});
